6db407e18128102c8d17a8d1e8775a49df45256a
Mauricio Bedoya <Mauricio.Bedoya@thomsonreuters.com>
2019-03-29 12:05:47 -0700

    l22

---
+++ Unit-1/l22/listing-2-2.go
@@ -0,0 +1,11 @@
+// My weight loss program.
+package main
+
+import "fmt"
+
+// main is the function where it all begins.
+
+func main() {
+	fmt.Printf("My weight on the surface of Mars is %v lbs,", 149.0*0.3783)
+	fmt.Printf(" and I would be %v years old.\n", 41*365/687)
+}
